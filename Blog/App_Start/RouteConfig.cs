a11a4e5482b153d02882091c563542e128c85177
Yangyang(JERRY) Xie <yxie23@hawk.iit.edu>
2015-12-28 15:44:05 -0600

    add pagination in gallery

--- Blog/App_Start/RouteConfig.cs
+++ Blog/App_Start/RouteConfig.cs
@@ -18,15 +18,21 @@ namespace Blog
                defaults: new { controller = "Article", action = "Index", ArticleID = UrlParameter.Optional }
             );
             routes.MapRoute(
+               name: "GalleryDefault",
+               url: "Gallery/{pageIndex}/{pageSize}",
+               defaults: new { controller = "Gallery", action = "Index", pageIndex = "0", pageSize = "12" }
+            );
+            routes.MapRoute(
                 name: "DashBoard",
                 url: "Admin/Dashboard/{action}/{parameters}",
-                defaults: new { controller = "DashBoard", action = "Index", parameters =UrlParameter.Optional}
+                defaults: new { controller = "DashBoard", action = "Index", parameters = UrlParameter.Optional }
                 );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
 
         }
     }
--- Blog/Controllers/GalleryController.cs
+++ Blog/Controllers/GalleryController.cs
@@ -12,12 +12,15 @@ namespace Blog.Controllers
         //
         // GET: /Gallery/
         private BlogContext db = new BlogContext();
-        public ActionResult Index()
+        public ActionResult Index(int pageIndex =0, int pageSize=12)
         {
             var publicImg = from x in db.PublicImagesVW
                             orderby x.PublicID descending
                             select x;
-            return View(publicImg);
+            ViewBag.pageIndex = pageIndex;
+            int count = (int)Math.Ceiling(publicImg.Count() / (double)pageSize);
+            ViewBag.pageCount = count;
+            return View(publicImg.Skip(pageIndex * pageSize).Take(pageSize));
         }
 
     }
