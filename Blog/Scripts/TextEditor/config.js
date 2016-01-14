0f92dd816791901acf87015bc2808f17a964b798
Yangyang(JERRY) Xie <yxie23@hawk.iit.edu>
2016-01-14 00:29:01 -0600

    add font color in ckeditor

--- Blog/Scripts/TextEditor/config.js
+++ Blog/Scripts/TextEditor/config.js
@@ -4,7 +4,8 @@
     config.font_names = '娃娃体/ChineseCute;' + '二分之一爱情/aiqing;' + "徐静蕾体/xujinglei;" + "圆体/yuanti;" + 'Arial/Arial, Helvetica, sans-serif;' +
 'Times New Roman/Times New Roman, Times, serif;' +'Initial/initial;'+
 'Verdana;';
-    config.extraPlugins = 'mathjax,link,font,justify,smiley';
+    config.extraPlugins = 'mathjax,link,font,justify,smiley,panelbutton,colorbutton';
+
     config.mathJaxLib = 'http://cdn.mathjax.org/mathjax/2.2-latest/MathJax.js?config=TeX-AMS_HTML';
     config.removeButtons = 'Underline,Subscript,Superscript,About';
     config.skin = 'kama';
