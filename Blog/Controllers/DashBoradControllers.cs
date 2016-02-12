90e323190f427b1cf4870b69c858f18a8b7214e7
Yangyang(JERRY) Xie <yxie23@hawk.iit.edu>
2016-02-12 00:01:53 -0600

    move functions to another controller

--- Blog/Controllers/DashBoradControllers.cs
+++ Blog/Controllers/DashBoradControllers.cs
@@ -37,7 +37,61 @@ namespace Blog.Controllers
                 return View();
             return RedirectToAction("Login", "Admin");
         }
+      [HttpGet]
+        public ActionResult ViewImage(int id)
+        {
+            if (Session["LoggedUserID"] == null)
+            {
+                return Content("Sorry ! please login first <a href='/Admin/Login'>Click Here to Login</a>");
+            }
+            var images = _db.Images.Where(a => a.UserID == id && a.isBlock == 0 && a.DeleteTime == null);
+            return View(images);
+        }
 
+
+        [HttpPost]
+        // ReSharper disable once InconsistentNaming
+        public ActionResult PicOperation(string ImageID, string operation)
+        {
+            var imageId = long.Parse(ImageID);
+            var oper = int.Parse(operation);
+            var id = long.Parse(Session["LoggedUserID"].ToString());
+            var query = from image in _db.Images
+                        where image.ImageID == imageId && image.UserID == id
+                        select image;
+            foreach (var one in query)
+            {
+                switch (oper)
+                {
+                    case 1:
+                        one.isPublish = 1;
+                        break;
+                    case 0:
+                        one.isPublish = 0;
+                        break;
+                    default:
+                        var archivePath = Server.MapPath("/Content/Users/" + id + "/DelArchive");
+                        if (!Directory.Exists(archivePath))
+                        {
+                            Directory.CreateDirectory(archivePath);
+                        }
+                        var splits = one.Url.Split('/');
+                        System.IO.File.Move(Server.MapPath(one.Url), Path.Combine(archivePath, splits[splits.Length - 1]));
+                        one.DeleteTime = DateTime.Now;
+                        break;
+                }
+            }
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            var images = _db.Images.Where(a => a.UserID == id && a.isBlock == 0 && a.DeleteTime == null);
+            return View("ViewImage", images);
+        }
 
         [HttpPost]
         public JsonResult FileUpload()
@@ -104,6 +158,54 @@ namespace Blog.Controllers
             var javaScriptSerializer = new JavaScriptSerializer();
             var jsonString = javaScriptSerializer.Serialize(retList);
             return Json(jsonString);
+        }
+
+        [HttpPost]
+        public ActionResult UploadImage(int id, HttpPostedFileWrapper upload)
+        {
+            string ret;
+            if (upload != null)
+            {
+                if (upload.ContentLength <= 1024 * 1024 * 5)
+                {
+                    var imageName = DateTime.Now.ToString("yyyyMMddHHmmss") + '_' + upload.FileName;
+                    if (!System.IO.Directory.Exists(Server.MapPath("/Content/Users/" + id)))
+                    {
+                        System.IO.Directory.CreateDirectory(Server.MapPath("/Content/users/" + id));
+                    }
+                    var path = System.IO.Path.Combine(Server.MapPath("/Content/users/" + id), imageName);
+                    var url = "/Content/users/" + id + "/" + imageName;
+                    upload.SaveAs(path);
+                    var resizer = new ImageResizer(@path);
+                    var thumbtailPath = System.IO.Path.Combine(Server.MapPath("~/Content/Users/" + id + "/thumbtail/"), upload.FileName);
+                    resizer.Resize(400, 400, ImageEncoding.Jpg90);
+                    if (!System.IO.Directory.Exists(Server.MapPath("~/Content/Users/" + id + "/thumbtail/")))
+                        System.IO.Directory.CreateDirectory(Server.MapPath("~/Content/Users/" + id + "/thumbtail/"));
+                    resizer.SaveToFile(@thumbtailPath);
+                    var image = new ImageViewModel();
+                    var imageMetaDate = new ImageMetaData(Server.MapPath(image.Url));
+                    imageMetaDate.FetchData();
+                    var metadata = imageMetaDate.GetMetaData();
+                    image.ContentType = upload.ContentType;
+                    image.UpdateDate = DateTime.Now;
+                    image.UserID = long.Parse(Session["LoggedUserID"].ToString());
+                    image.Url = url;
+                    image.FileName = imageName;
+                    _db.Images.Add(image);
+                    _db.ImageMetaData.Add(metadata);
+                    _db.SaveChanges();
+                    ret = url;
+                }
+                else
+                {
+                    ret = "<p style=\"color:red\">image exceed the maximum 5MB</p>";
+                }
+            }
+            else
+            {
+                ret = "<p style=\"color:red\">image is empty</p>";
+            }
+            return Content(ret);
         }
 
         public class RetJsonModel
--- BlogTest/HomeTest.cs
+++ BlogTest/HomeTest.cs
@@ -25,15 +25,43 @@ namespace BlogTest
             Assert.IsNotNull(actResult);
             var viewModel = actResult.ViewData.Model as IEnumerable<ArticleAbstract>;
             Assert.IsNotNull(viewModel);
-            ArticleAbstract[] enumerable = viewModel as ArticleAbstract[] ?? viewModel.ToArray();
+            var enumerable = viewModel as ArticleAbstract[] ?? viewModel.ToArray();
             Assert.LessOrEqual(enumerable.Length,10);
-            ArticleAbstract[] orderedEnumerable = enumerable.OrderBy(a => a.PostDate ).ToArray();
+            var orderedEnumerable = enumerable.OrderBy(a => a.PostDate ).ToArray();
             //Test Wether it is ordered
-            for (int i = 0; i < enumerable.Length; i++)
+            for (var i = 0; i < enumerable.Length; i++)
             {
                 Assert.AreEqual(enumerable[i].ArticleId,orderedEnumerable[enumerable.Length-1-i].ArticleId);
             }
-            foreach (ArticleAbstract one in enumerable)
+            foreach (var one in enumerable)
+            {
+                Assert.IsNotNull(one.ArticleId);
+                Assert.IsNotNull(one.AuthorId);
+                Assert.IsNotNull(one.Title);
+                Assert.IsNotNull(one.SubTitle);
+                Assert.IsNotNull(one.AuthorName);
+                Assert.IsNotNull(one.PostDate);
+            }
+        }
+        [Test]
+        public void TestPartialHomeIndex()
+        {
+            const int currentPage = 1;
+            const int lastPage = 1;
+            _home.Index();
+            var actResult = _home.Index(currentPage, lastPage) as PartialViewResult;
+            Assert.IsNotNull(actResult);
+            var viewModel = actResult.ViewData.Model as IEnumerable<ArticleAbstract>;
+            Assert.IsNotNull(viewModel);
+            var enumerable = viewModel as ArticleAbstract[] ?? viewModel.ToArray();
+            Assert.LessOrEqual(enumerable.Length, 10);
+            var orderedEnumerable = enumerable.OrderBy(a => a.PostDate).ToArray();
+            //Test Wether it is ordered
+            for (var i = 0; i < enumerable.Length; i++)
+            {
+                Assert.AreEqual(enumerable[i].ArticleId, orderedEnumerable[enumerable.Length - 1 - i].ArticleId);
+            }
+            foreach (var one in enumerable)
             {
                 Assert.IsNotNull(one.ArticleId);
                 Assert.IsNotNull(one.AuthorId);
